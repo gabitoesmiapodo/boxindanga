6105446b0cbea2ff48cffcefec0f654ee3828c11
Gabito Esmiapodo <gabriel@bootnode.dev>
2025-04-09 02:02:38 -0300

    feat: added methods and properties to make animations go faster

--- src/playerAnimation.ts
+++ src/playerAnimation.ts
@@ -3,10 +3,13 @@ import type { Player } from './player'
 import { drawSprite } from './utils'
 
 export class PlayerAnimation {
+  private readonly defaultSpeed = 1
+
   private currentAnimation: Animation
   private currentFrameIndex = 0
   private animationTimeElapsed = 0
   private player: Player
+  private speedDivider = this.defaultSpeed
 
   constructor(player: Player, currentAnimation: Animation) {
     this.currentAnimation = currentAnimation
@@ -17,6 +20,7 @@ export class PlayerAnimation {
     this.animationTimeElapsed = 0
     this.currentFrameIndex = 0
     this.currentAnimation = this.player.isFacingRight() ? faceRightIdle : faceLeftIdle
+    this.speedDivider = this.defaultSpeed
     this.player.setState('idle')
   }
 
@@ -34,7 +38,10 @@ export class PlayerAnimation {
     // drawBoundingBoxes(this.player)
     this.animationTimeElapsed += dt
 
-    if (this.animationTimeElapsed >= this.currentAnimation[this.currentFrameIndex].speed) {
+    if (
+      this.animationTimeElapsed >=
+      this.currentAnimation[this.currentFrameIndex].speed / this.speedDivider
+    ) {
       this.animationTimeElapsed = 0
 
       if (this.isAtAnimationEnd()) {
@@ -53,7 +60,19 @@ export class PlayerAnimation {
     return this.currentAnimation
   }
 
+  public setFastForward() {
+    this.speedDivider = 2
+  }
+
+  public isFastForwarding() {
+    return this.speedDivider === 2
+  }
+
   public getCurrentFrameIndex() {
     return this.currentFrameIndex
+  }
+
+  public setCurrentFrameIndex(currentFrameIndex: number) {
+    this.currentFrameIndex = currentFrameIndex
   }
 }
