e8c8696d6243c693d4380b06a72091150ac0b20c
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2026-02-10 10:15:40 -0300

    refactor: align audio event bus types

--- src/include/audioEvents.ts
+++ src/include/audioEvents.ts
@@ -1,33 +1,21 @@
-export type AudioEventHandler = () => void
+export type AudioEvent = 'audio:gloveHit' | 'audio:headHit' | 'audio:roundEnd'
+type Handler = () => void
 
-class AudioEvents {
-  private handlers = new Map<string, Set<AudioEventHandler>>()
-
-  on(event: string, handler: AudioEventHandler) {
-    let eventHandlers = this.handlers.get(event)
-    if (!eventHandlers) {
-      eventHandlers = new Set<AudioEventHandler>()
-      this.handlers.set(event, eventHandlers)
-    }
-
-    eventHandlers.add(handler)
-
-    return () => {
-      eventHandlers?.delete(handler)
-      if (eventHandlers && eventHandlers.size === 0) {
-        this.handlers.delete(event)
-      }
-    }
+class AudioBus {
+  private handlers: Record<AudioEvent, Set<Handler>> = {
+    'audio:gloveHit': new Set(),
+    'audio:headHit': new Set(),
+    'audio:roundEnd': new Set(),
   }
 
-  emit(event: string) {
-    const eventHandlers = this.handlers.get(event)
-    if (!eventHandlers) return
+  on(event: AudioEvent, handler: Handler) {
+    this.handlers[event].add(handler)
+    return () => this.handlers[event].delete(handler)
+  }
 
-    for (const handler of eventHandlers) {
-      handler()
-    }
+  emit(event: AudioEvent) {
+    for (const handler of this.handlers[event]) handler()
   }
 }
 
-export const audioEvents = new AudioEvents()
+export const audioEvents = new AudioBus()
