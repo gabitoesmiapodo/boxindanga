32464ef140d50a10e655c7397f296783b4f7677e
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2026-02-09 21:57:44 -0300

    test: reject HitBlocked event object

--- src/include/animationStateMachine.type-test.ts
+++ src/include/animationStateMachine.type-test.ts
@@ -3,6 +3,6 @@ import type { AnimationStateMachine } from './animationStateMachine'
 type EventType = Parameters<AnimationStateMachine['onEvent']>[0]
 
 // @ts-expect-error HitBlocked should not be a valid event type.
-const invalidEvent: EventType = 'HitBlocked'
+const invalidEvent: EventType = { type: 'HitBlocked' }
 
 void invalidEvent
